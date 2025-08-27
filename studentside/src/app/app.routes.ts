5071c9af2f2f82074f766bf8bf14c49053bdc1f0
Samyuktha <132467295+Samy-uktha@users.noreply.github.com>
2025-08-27 13:11:24 +0530

    Delete studentside directory

--- studentside/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { HomeComponent } from './home/home.component';
-import { InputComponent } from './input/input.component';
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-    { path: '', component: InputComponent }, // Default route is the login page
-    { path: 'dashboard', component: HomeComponent },
-];
